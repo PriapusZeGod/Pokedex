84239c5d7e8d1689b1a02bb863554e01180a1f04
Chiril Luncasu <chiril.luncasu@gmail.com>
2023-04-23 17:13:36 +0200

    Deployed imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { RouterProvider, createHashRouter, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <nav>
-      <Link to="/" element={<Pokedex />}></Link>
+      <Link to="/" element={<Home />}></Link>
       <Link to="/:pokemonId" element={<PokemonDetails />}>
         About
       </Link>
