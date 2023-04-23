6eed120994959d20d00999973c0731b83c36da13
Chiril Luncasu <chiril.luncasu@gmail.com>
2023-04-23 17:19:59 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { RouterProvider, createHashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Home, About } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const router = createHashRouter([
