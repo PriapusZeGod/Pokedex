c22beb4e60f7f6ceaeacd3c5d98d7742b8831b5d
Chiril Luncasu <chiril.luncasu@gmail.com>
2023-04-23 16:49:48 +0200

    Updates

---
+++ precache-manifest.69206166d20ff30816803dd18dd28aea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37b09e9b88e05b600ee9",
+    "url": "/Pokedex/static/css/main.2341cfdb.chunk.css"
+  },
+  {
+    "revision": "37b09e9b88e05b600ee9",
+    "url": "/Pokedex/static/js/main.37b09e9b.chunk.js"
+  },
+  {
+    "revision": "5a3e1acf064476f56861",
+    "url": "/Pokedex/static/js/1.5a3e1acf.chunk.js"
+  },
+  {
+    "revision": "b3468cd3de89372a17d9",
+    "url": "/Pokedex/static/js/runtime~main.b3468cd3.js"
+  },
+  {
+    "revision": "d1a96ade03d2c5149d0e8a8e81933a13",
+    "url": "/Pokedex/index.html"
+  }
+];
